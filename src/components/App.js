3c6177b322b224b0a571dc120c8e888cc65ed8bc
MAhri Atdayeva <mahriaa88@gmail.com>
2021-01-02 18:42:31 +0000

    creating ReactProject practice router

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="card">
+        <h1>Video Player</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
